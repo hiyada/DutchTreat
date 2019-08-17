1c50d5af80a736d67a4ee15fb6226bd137a7aee3
hiyada <hiyada@users.noreply.github.com>
2019-08-17 14:40:58 -0500

    added js file

---
+++ DutchTreat/wwwroot/js/index.js
@@ -0,0 +1,11 @@
+﻿var theForm = document.getElementById("the-form");
+
+theForm.hidden = true;
+
+var button = document.getElementById("buy-button");
+button.addEventListener("click", function ()){
+    console.log("Buying Item");
+};
+
+var productInfo = document.getElementsByClassName("product-props");
+var listItems = productInfo.item[0].children;
