fe2f6143e95f2ccc639d0cea2d8b38b7810dce28
hiyada <hiyada@users.noreply.github.com>
2019-08-21 06:47:05 -0500

    41 next

--- DutchTreat/Controllers/AppController.cs
+++ DutchTreat/Controllers/AppController.cs
@@ -1,4 +1,5 @@
-﻿using DutchTreat.ViewModels;
+﻿using DutchTreat.Services;
+using DutchTreat.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -10,6 +11,13 @@ namespace DutchTreat.Controllers
 {
     public class AppController : Controller
     {
+        private readonly IMailService _mailService;
+
+        public AppController(IMailService mailService)
+        {
+            _mailService = mailService;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -26,7 +34,9 @@ namespace DutchTreat.Controllers
         {
             if (ModelState.IsValid)
             {
-                return View();
+                _mailService.SendMessage("dyson.allie@gmail.com", model.Subject, $"From: {model.Name} - {model.Email}, message: {model.Message}");
+                ViewBag.UserMessage = "Mail Sent";
+                ModelState.Clear();
             }
             else
             {
